d3382ca0df7e9a14f6d53e5b52048d6900ac75b9
Ubuntu <ubuntu@ip-172-26-3-176.eu-central-1.compute.internal>
2020-04-30 21:00:29 +0000

    changes in settings and added static files

--- src/margemfrontal/wsgi.py
+++ src/margemfrontal/wsgi.py
@@ -1,16 +1,24 @@
-"""
-WSGI config for margemfrontal project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'margemfrontal.settings')
-
-application = get_wsgi_application()
+import os 
+import time 
+import traceback 
+import signal 
+import sys 
+ 
+from django.core.wsgi import get_wsgi_application 
+ 
+sys.path.insert(0, '/home/ubuntu/djangoBootstrap1/src/margemfrontal')
+sys.path.insert(1, '/home/ubuntu/djangoBootstrap1/src')
+#sys.path.append('/home/ubuntu/djangoBootstrap1/src') 
+# adjust the Python version in the line below as needed 
+sys.path.append('/home/ubuntu/djangoBootstrap1/src/venv/lib/python3.6/site-packages') 
+ 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "margemfrontal.settings") 
+ 
+try: 
+    application = get_wsgi_application() 
+except Exception: 
+    # Error loading applications 
+    if 'mod_wsgi' in sys.modules: 
+        traceback.print_exc() 
+        os.kill(os.getpid(), signal.SIGINT) 
+        time.sleep(2.5) 
